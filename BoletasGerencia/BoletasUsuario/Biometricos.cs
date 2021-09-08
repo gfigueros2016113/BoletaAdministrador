267540a92a0add9c1cded33ff13986f0b3045764
Gabriel <gfigueros-2016113@kinal.edu.gt>
2021-09-08 11:32:52 -0600

    Vista de reportes
    Vista de aplicacion de reporteria de marcas

---
+++ BoletasGerencia/BoletasUsuario/Biometricos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoletasUsuario
+{
+    public partial class Biometricos : Form
+    {
+        public Biometricos()
+        {
+            InitializeComponent();
+        }
+        private void marcas_btn_Click(object sender, EventArgs e)
+        {
+            Marcas marcas = new Marcas();
+            marcas.Show();
+        }
+
+        private void Biometricos_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
